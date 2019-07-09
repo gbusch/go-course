03554f39fb92c99391d008005d00b9d8ef5d0865
Gerold Busch <gerold.busch@gmail.com>
2019-07-09 08:29:33 +0200

    change raceCondition script to make pipeline pass

--- raceCondition/raceCondition.go
+++ raceCondition/raceCondition.go
@@ -4,21 +4,17 @@ import (
 	"fmt"
 )
 
-func getNumber() int {
-	var i int
-	/* race condition = outcome depends on non-deterministic ordering (interleavings)
-	here, we initialized i, so it is 0. At the end of the function we return it.
-	In the middle of the function, we call another function that sets i to 42.
-	Depending on the ordering of the execution, the value of i is either set to 42 and then returned
-	(so it will print 42 in the main function)
-	or it is returned already before the value is changed to 42
-	(in this case it will print 0 in the main function)*/
-	go func() {
-		i = 42
-	}()
-	return i
-}
-
 func main() {
-	fmt.Println(getNumber())
+	var x int
+	/* race condition = outcome depends on non-deterministic ordering (interleavings)
+	Here we initialize an integer x (so it is 0). At the end of the function we increase the value,
+	so it will be 1, and print it.
+	In the middle, we add another function that should print the value.
+	Depending on the ordering of the execution, this is either exectured before the increase of the number
+	or afterwards. This result in two different results.*/
+	go func() {
+		fmt.Println(x)
+	}()
+	x++
+	fmt.Println(x)
 }
